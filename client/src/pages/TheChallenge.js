afd004aed7c4c55b62acce06a10d4463ea944458
Victoria Lee <victoriacmlee@gmail.com>
2021-02-02 21:49:09 -0500

    update the challenge and game canvas

--- client/src/pages/TheChallenge.js
+++ client/src/pages/TheChallenge.js
@@ -4,13 +4,8 @@ import GameCanvas from "../components/GameCanvas"
 import GameOver from "../components/GameOver"
 import "./pages.css"
 
-function Game({ userState , setUserState}) {
-    // console.log(gameScore)
-    // console.log(highScore)
-    // console.log({...userState})
-    // setUserState({... userState: email: "oo"})
-    // console.log(setUserState)
-
+function Game({ userState, setUserState }) {
+    
     // score state
     const [gameStart, setGameStart] = useState(false);
     const [gameOver, setGameOver] = useState(false);
@@ -18,35 +13,28 @@ function Game({ userState , setUserState}) {
     const [gameScore, setGameScore] = useState(0);
     const [Message, setMessage] = useState("");
 
-    // check if high score to save to database
-    // function checkHighScore() {
-    //     // save high score
-    //     if (gameScore > highScore) {
-    //         setUserState(userState.highScore: gameScore)
-    //     };
-    //     reutrn
-    // };
 
     // start button toggle
     function toggle() {
 
         setGameStart(!gameStart);
         console.log(gameStart);
-
-        if (!gameStart) {
-            // handleStartTime()
-        }
-    }
+    };
 
     //   timer countdown
     useEffect(() => {
+
         let counter = null;
+
         if (gameStart) {
             counter = setInterval(() => {
+
                 if (timer > 0) {
+
                     setTimer((timer) => timer - 1);
+
                 } else {
-                    // handleGameOver()
+                
                     console.log("gameover");
                     setGameStart(false);
                     setGameOver(true)
@@ -55,29 +43,32 @@ function Game({ userState , setUserState}) {
             }, 1000);
         }
         return () => clearInterval(counter);
+        
     }, [gameStart, timer]);
 
 
     return (
 
         <div className="gamewrap" >
-           {gameStart && <div>{gameScore} Point</div>}
-        {gameStart && <div>{timer} s</div>}
-            {gameStart && <div>{Message}</div>}
+
+            {gameStart && <div> {gameScore} Point</div>}
+            {gameStart && <div>{timer} s</div>}
+            <div style={{ height: "50px", position: "relative" }}>
+                {gameStart && <div style={{ position: "absolute", left: "48%" }}>{Message}</div>}
+            </div>
 
             <button className={`${gameStart ? true : false}`} onClick={toggle} >
                 {gameStart ? "Exit" : "Start"}
             </button>
 
             <div >
-                {gameOver && <GameOver gameScore={gameScore}/>}
+                {gameOver && <GameOver gameScore={gameScore} userState={userState} />}
             </div>
 
-            {gameStart && <GameCanvas gameScore={gameScore} setGameScore={setGameScore} setMessage={setMessage} />}
+            {gameStart && <GameCanvas userState={userState} setUserState={setUserState} gameScore={gameScore} setGameScore={setGameScore} setMessage={setMessage} />}
 
         </div>
     );
-
 };
 
 export default Game;
